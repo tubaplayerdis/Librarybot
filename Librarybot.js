22ff044e9b687d79b925ef67d28d55f0d1d5e963
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2021-01-18 18:29:32 -0600

    Update Librarybot.js

--- Librarybot.js
+++ Librarybot.js
@@ -69,7 +69,25 @@ client.on('message', message =>{
             } else {
                 message.channel.send("Error detected, channel not NSFW.");
             }
-        } 
+        } else if (message.content.startsWith('*L1v1')){
+            message.channel.send('https://tenor.com/view/horny-jail-bonk-dog-hit-head-stop-being-horny-gif-17298755')
+        } else if (message.content.startsWith('*L1v2')){
+            message.channel.send('https://cdn.discordapp.com/attachments/717384499041665036/731649428150157362/Yandere_Dev_Viva_La_Vida.mp4')
+        } else if (message.content.startsWith('*L1v3')){
+            message.channel.send('https://cdn.discordapp.com/attachments/717384499041665036/739930648923472003/image0.jpg')
+        } else if (message.content.startsWith('*L1v4')){
+            message.channel.send('https://cdn.discordapp.com/attachments/738929450959503361/743942710297362472/video0.mp4')
+        } else if (message.content.startsWith('*L1v5')){
+            message.channel.send('https://cdn.discordapp.com/attachments/579937698039726081/740621521784930324/Renai_Circulation.mp4')
+        } else if (message.content.startsWith('*L1v6')){
+            message.channel.send('https://cdn.discordapp.com/attachments/717384185194479681/740685228972179596/video0_13.mov')
+        } else if (message.content.startsWith('*L1v7')){
+            message.channel.send('https://cdn.discordapp.com/attachments/740191587149021204/740295265000357995/image0.jpg')
+        } else if (message.content.startsWith('*L1v8')){
+            message.channel.send('https://cdn.discordapp.com/attachments/723195804906422362/729224124039430194/video0_21-2.mov')
+        } else if (message.content.startsWith('*rickroll2')){
+            message.channel.send('https://cdn.discordapp.com/attachments/745505740831064127/745634842347044934/why.mp4')
+        }
         
         
         
